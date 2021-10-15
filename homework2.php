51af1bcab2feeb1e58e2931be4ae5af2ee0f6c56
Artyom Rogov <artyomrogov2012@gmail.com>
2021-10-15 19:02:18 +0200

    homework2: добавлена проверка на корректность введенных данных, исправлена неточность сравнения, удален лишний цикл, поправлено оформление

--- homework2.php
+++ homework2.php
@@ -1,17 +1,27 @@
 <?php
 require "movies (1).php";
 $userAge = readline("Input your age: ");
-$allowedMovies = [];
 
-foreach ($movies as $movie) {
-	if ($userAge > $movie["age_restriction"]) {
-		$allowedMovies[] = $movie;
-	}
+if (is_numeric($userAge))
+{
+	showAllowedMovies($movies, $userAge);
+}
+else
+{
+	echo "Age is incorrect.";
 }
 
-$i = 1;
-foreach ($allowedMovies as $movie) {
-	echo "$i. $movie[title] ($movie[release_year]), $movie[age_restriction]+. Rating - $movie[rating]";
-	echo "\n";
-	$i++;
-}
+function showAllowedMovies($movies, $userAge)
+ {
+	 $i = 1;
+	 foreach ($movies as $movie)
+	 {
+		 if ((int) $userAge >= $movie["age_restriction"])
+		 {
+			 echo "$i. $movie[title] ($movie[release_year]), $movie[age_restriction]+. Rating - $movie[rating]";
+			 echo "\n";
+			 $i++;
+		 }
+	 }
+ }
+
